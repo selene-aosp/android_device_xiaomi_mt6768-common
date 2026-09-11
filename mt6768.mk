3cf7feb391c7f37b3a8c4e5ee8fe63c447901403
Hasan Basri OY <h.basri3460@gmail.com>
2026-09-11 11:17:53 +0300

    mt6768-common: Support VAB devices.
    Welcome selene, earth, fire and etc
    
    Signed-off-by: hasan6034 <h.basri3460@gmail.com>

--- mt6768.mk
+++ mt6768.mk
@@ -10,8 +10,13 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Non_ab_device
-$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+ifeq ($(AB_OTA_UPDATER), true)
+    # A/B
+    $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+else
+    # Non_ab_device
+    $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+endif
 
 # Default dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
